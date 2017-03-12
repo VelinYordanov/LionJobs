344fd222c83ff93cc3cb7ce2c7edb1c5bc78c58b
VelinYordanov <velin.iordanov@gmail.com>
2017-03-12 17:45:39 +0200

    Added scaffold MVC project.

---
+++ LionJobs/LionJobs.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LionJobs.Web.MvcApplication" Language="C#" %>
---
+++ LionJobs/LionJobs.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LionJobs.Web.Startup))]
+namespace LionJobs.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
