1bcfab1a699d9d0f4886a90b33a9e44ef591dc88
VelinYordanov <velin.iordanov@gmail.com>
2017-03-14 12:03:00 +0200

    Added repository pattern.

---
+++ LionJobs/LionJobs.Data.Common/IEfRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace LionJobs.Data.Common
+{
+    public interface IEfRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T1> GetAll<T1>(Expression<Func<T, bool>> filterExpression, Expression<Func<T, T1>> selectExpression);
+
+        // For queryable collections
+        //IQueryable<T> GetAll { get; }
+
+        T GetById(object id);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+    }
+}
