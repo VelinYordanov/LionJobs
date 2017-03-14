b461a24c93b8a792922a53ecf98fd60abaeec916
VelinYordanov <velin.iordanov@gmail.com>
2017-03-14 12:09:21 +0200

    Added unit of work.

---
+++ LionJobs/LionJobs.Data.Common/EfUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace LionJobs.Data.Common
+{
+    public class EfUnitOfWork : IUnitOfWork
+    {
+        private DbContext context;
+
+        public EfUnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public void SaveChanges()
+        {
+            this.context.SaveChanges();
+        }
+    }
+}
---
+++ LionJobs/LionJobs.Data.Common/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LionJobs.Data.Common
+{
+    public interface IUnitOfWork
+    {
+        void SaveChanges();
+    }
+}
