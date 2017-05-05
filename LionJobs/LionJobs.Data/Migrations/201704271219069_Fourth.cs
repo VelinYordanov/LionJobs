753f1204ed3630b0e6ab805e0f64b62296c22483
VelinYordanov <velin.iordanov@gmail.com>
2017-05-05 21:34:25 +0300

    Added forgotten migrations from last commit.

---
+++ LionJobs/LionJobs.Data/Migrations/201704271219069_Fourth.cs
@@ -0,0 +1,16 @@
+namespace LionJobs.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Fourth : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
